82cfb0ba4e771cab07e3b58967eda9aaa7ff6e60
GDWR <gregory.dwr@gmail.com>
2020-12-11 18:17:11 +0000

    Day 9 pt.2

--- Day 9 - Encoding Error/script.py
+++ Day 9 - Encoding Error/script.py
@@ -11,8 +11,8 @@ def addCheck(nums, target):
 window = 24
 
 while window < len(puzzle_input) - 1:
-    subList = puzzle_input[window-24:window+1]
-    target = puzzle_input[window+1]
+    subList = puzzle_input[window - 24:window + 1]
+    target = puzzle_input[window + 1]
 
     perms = list(itertools.permutations(subList, 2))
     if not any(map(addCheck, perms, [target for _ in range(len(perms))])):
@@ -20,3 +20,18 @@ while window < len(puzzle_input) - 1:
         break
 
     window += 1
+
+
+l, r = 0, 0
+
+while window < len(puzzle_input) - 1:
+    subList = puzzle_input[l:r]
+    subSum = sum(subList)
+    if subSum == target:
+        subList.sort()
+        print(f"Part Two: {subList[0] + subList[-1]}")
+        break
+    elif subSum > target:
+        l += 1
+    else:
+        r += 1
